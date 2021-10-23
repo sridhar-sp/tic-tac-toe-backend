f3adc2b14a9d4acf5a4383fbdb127e5d076662ab
sridhar-sp <sridharthechosenone@gmail.com>
2021-10-23 10:36:47 +0530

    Add board manager

---
+++ src/board/boardManager.go
@@ -0,0 +1,153 @@
+package board
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+type Element int
+
+const (
+	EMPTY_CELL  Element = 0
+	CROSS_CELL  Element = 1
+	NOUGHT_CELL Element = 2
+)
+
+type Board [][]Element
+
+type boardRepo struct {
+	boardContent Board
+	rowSize      int
+	columnSize   int
+	totalMoves   int
+	randomizer   *rand.Rand
+}
+
+type BoardManger interface {
+	New() *boardRepo
+	GetBoardSize() int
+	GetCell(row int, column int) Element
+	IsEmptyCell(row int, column int) bool
+	GetBoard() Board
+	ResetBoard()
+	Move(row int, column int, element Element)
+	IsGameStarted() bool
+	PlayComputerMove()
+	PrintBoard()
+	getEmptyCellIndexes() []Element
+	getFlatIndexFromRowAndColumn() int
+	GetRowAndColumnFromFlatIndex(cellIndex int) (int, int)
+}
+
+func New(rowSize int, columnSize int) *boardRepo {
+	return &boardRepo{
+		boardContent: getEmptyBoard(rowSize, columnSize),
+		rowSize:      rowSize,
+		columnSize:   columnSize,
+		totalMoves:   0,
+		randomizer:   rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+func (b boardRepo) GetBoardSize() int {
+	return b.rowSize * b.columnSize
+}
+
+func (b boardRepo) GetCell(row int, column int) Element {
+	return b.boardContent[row][column]
+}
+
+func (b boardRepo) IsEmptyCell(row int, column int) bool {
+	return b.GetCell(row, column) == EMPTY_CELL
+}
+
+func (b *boardRepo) GetBoard() Board {
+	return b.boardContent
+}
+
+func (b *boardRepo) ResetBoard() {
+	b.boardContent = getEmptyBoard(b.rowSize, b.columnSize)
+	b.totalMoves = 0
+}
+
+func (b *boardRepo) Move(row int, column int, element Element) {
+	b.boardContent[row][column] = element
+	b.totalMoves++
+}
+
+func (b *boardRepo) IsGameStarted() bool {
+	for row := 0; row < b.rowSize; row++ {
+		for column := 0; column < b.columnSize; column++ {
+			if b.GetCell(row, column) != EMPTY_CELL {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (b boardRepo) PlayComputerMove(element Element) (int, int) {
+	emptyCellIndexs := b.getEmptyCellIndexes()
+	availableSpaces := len(emptyCellIndexs)
+
+	if availableSpaces == 0 {
+		log.Println("No empty space for computer to make a move")
+		return -1, -1
+	}
+
+	indexToSelect := b.randomizer.Intn(availableSpaces)
+
+	rowIndex, colIndex := b.GetRowAndColumnFromFlatIndex(emptyCellIndexs[indexToSelect])
+	b.Move(rowIndex, colIndex, element)
+	return rowIndex, colIndex
+}
+
+func (b boardRepo) GetRowAndColumnFromFlatIndex(cellIndex int) (int, int) {
+	rowIndex := cellIndex / b.rowSize
+	colIndex := cellIndex % b.columnSize
+	return rowIndex, colIndex
+}
+
+func (b boardRepo) getEmptyCellIndexes() []int {
+	emptyIndexes := make([]int, 0, b.GetBoardSize())
+
+	for row := 0; row < b.rowSize; row++ {
+		for column := 0; column < b.columnSize; column++ {
+			if b.GetCell(row, column) == EMPTY_CELL {
+				emptyIndexes = append(emptyIndexes, b.getFlatIndexFromRowAndColumn(row, column))
+			}
+		}
+	}
+
+	return emptyIndexes
+}
+
+func (b boardRepo) getFlatIndexFromRowAndColumn(row int, column int) int {
+	return row*b.rowSize + column
+}
+
+func (b boardRepo) PrintBoard() {
+
+	for rowIndex := 0; rowIndex < b.rowSize; rowIndex++ {
+		for columnIndex := 0; columnIndex < b.columnSize; columnIndex++ {
+			fmt.Print(b.boardContent[rowIndex][columnIndex])
+			fmt.Printf(" ")
+		}
+		fmt.Println("")
+	}
+}
+
+func getEmptyBoard(rowSize int, columSize int) [][]Element {
+	var boardData = make([][]Element, rowSize)
+
+	for row := 0; row < rowSize; row++ {
+		boardData[row] = make([]Element, columSize)
+		for column := 0; column < columSize; column++ {
+			boardData[row][column] = EMPTY_CELL
+		}
+	}
+
+	return boardData
+}
--- src/main.go
+++ src/main.go
@@ -5,37 +5,18 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	svg "github.com/ajstarks/svgo"
 	"github.com/sridhar-sp/tic-tac-toe-backend/src/assets"
+	"github.com/sridhar-sp/tic-tac-toe-backend/src/board"
 )
 
 const REDIRECT_URL = "https://github.com/sridhar-sp/tic-tac-toe"
 
-const ROW_SIZE = 3
-const COLUMN_SIZE = 3
-const BOARD_SIZE = ROW_SIZE * COLUMN_SIZE
-
-var random = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-type Element int
-
-const EMPTY_CELL Element = 0
-const CROSS_CELL Element = 1
-const NOUGHT_CELL Element = 2
-
-var board [][]Element = getEmptyBoard(ROW_SIZE, COLUMN_SIZE)
-
-var activities = []string{"* Game is yet to begin"}
-
-func resetBoard() {
-	board = getEmptyBoard(ROW_SIZE, COLUMN_SIZE)
-}
+var activities = []string{}
 
 func parseAndValidateCellIndexFromRequest(req *http.Request) (int, error) {
 
@@ -47,7 +28,7 @@ func parseAndValidateCellIndexFromRequest(req *http.Request) (int, error) {
 
 	log.Println("cell index is", cellIndex)
 
-	if cellIndex >= BOARD_SIZE {
+	if cellIndex >= boardManager.GetBoardSize() {
 		return -1, errors.New("cell index is invalid")
 	}
 
@@ -64,83 +45,6 @@ func writeCommonHeaders(responseWriter http.ResponseWriter) {
 func writeEmptyTextResponse(responseWriter http.ResponseWriter) {
 	responseWriter.Header().Set("Content-Type", "text/html")
 	responseWriter.Write([]byte(""))
-}
-
-func getRowAndColumnIndex(cellIndex int) (int, int) {
-	rowIndex := cellIndex / ROW_SIZE
-	colIndex := cellIndex % COLUMN_SIZE
-	return rowIndex, colIndex
-}
-
-func getFlatIndexFromRowAndColumn(row int, column int, rowSize int, columnSize int) int {
-	return row*rowSize + column
-}
-
-func isGameStarted(gameBoard [][]Element) bool {
-	for i := 0; i < len(gameBoard); i++ {
-		for j := 0; j < len(gameBoard[i]); j++ {
-			if gameBoard[i][j] != EMPTY_CELL {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func getEmptyBoard(rowSize int, columSize int) [][]Element {
-	var boardData = make([][]Element, rowSize)
-
-	for row := 0; row < rowSize; row++ {
-		boardData[row] = make([]Element, columSize)
-		for column := 0; column < columSize; column++ {
-			boardData[row][column] = EMPTY_CELL
-		}
-	}
-
-	return boardData
-}
-
-func getEmptyCellIndexes(gameBoard [][]Element) []int {
-	emptyIndexes := make([]int, 0, BOARD_SIZE)
-
-	rowSize := len(gameBoard)
-	for row := 0; row < rowSize; row++ {
-		columnSize := len(gameBoard[row])
-		for column := 0; column < columnSize; column++ {
-			if gameBoard[row][column] == EMPTY_CELL {
-				emptyIndexes = append(emptyIndexes, getFlatIndexFromRowAndColumn(row, column, rowSize, columnSize))
-			}
-		}
-	}
-
-	return emptyIndexes
-}
-
-func playComputerMove(gameBoard [][]Element) (int, int) {
-	emptyCellIndexs := getEmptyCellIndexes(gameBoard)
-	availableSpaces := len(emptyCellIndexs)
-	if availableSpaces == 0 {
-		log.Println("No empty space for computer to make a move")
-		return -1, -1
-	}
-
-	indexToSelect := random.Intn(availableSpaces)
-
-	rowIndex, colIndex := getRowAndColumnIndex(emptyCellIndexs[indexToSelect])
-	gameBoard[rowIndex][colIndex] = NOUGHT_CELL
-	return rowIndex, colIndex
-}
-
-// Todo remove this method once initial devlopment is over
-func printCells(gameBoard [][]Element) {
-
-	for i := 0; i < len(gameBoard); i++ {
-		for j := 0; j < len(gameBoard[i]); j++ {
-			fmt.Print(gameBoard[i][j])
-			fmt.Printf(" ")
-		}
-		fmt.Println("")
-	}
 }
 
 func getPort() string {
@@ -202,6 +106,9 @@ func clearActivities() {
 }
 
 func getActivities() []string {
+	if len(activities) == 0 {
+		return []string{"* Game is yet to begin"}
+	}
 	return activities
 }
 
@@ -225,13 +132,13 @@ func onRenderCell(responseWriter http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	rowIndex, colIndex := getRowAndColumnIndex(cellIndex)
+	rowIndex, colIndex := boardManager.GetRowAndColumnFromFlatIndex(cellIndex)
 
-	switch cellValue := board[rowIndex][colIndex]; cellValue {
-	case CROSS_CELL:
+	switch cellValue := boardManager.GetCell(rowIndex, colIndex); cellValue {
+	case board.CROSS_CELL:
 		log.Println("Render cross cell")
 		responseWriter.Write(assetManager.GetSvg(assets.CrossImage))
-	case NOUGHT_CELL:
+	case board.NOUGHT_CELL:
 		log.Println("Render nougt cell")
 		responseWriter.Write(assetManager.GetSvg(assets.NoughtImage))
 	default:
@@ -251,13 +158,17 @@ func onClickCell(responseWriter http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	rowIndex, colIndex := getRowAndColumnIndex(cellIndex)
+	rowIndex, colIndex := boardManager.GetRowAndColumnFromFlatIndex(cellIndex)
+	if boardManager.IsEmptyCell(rowIndex, colIndex) {
+		boardManager.Move(rowIndex, colIndex, board.CROSS_CELL)
+		computerMoveRowIndex, computerMoveColIndex := boardManager.PlayComputerMove(board.NOUGHT_CELL)
 
-	board[rowIndex][colIndex] = CROSS_CELL
-	computerMoveRowIndex, computerMoveColIndex := playComputerMove(board)
-
-	updateActivities(fmt.Sprintf("* Player X clicked cell [%d,%d]", rowIndex, colIndex))
-	updateActivities(fmt.Sprintf("* Player O clicked cell [%d,%d]", computerMoveRowIndex, computerMoveColIndex))
+		// updateActivities(fmt.Sprintf("* Player 'X' clicked cell [%d , %d]", rowIndex, colIndex))
+		// updateActivities(fmt.Sprintf("* Player 'O' clicked cell [%d , %d]", computerMoveRowIndex, computerMoveColIndex))
+		updateActivities(fmt.Sprintf("* Player 'X' clicked cell [%d , %d] and player 'O' clicked cell [%d , %d]", rowIndex, colIndex, computerMoveRowIndex, computerMoveColIndex))
+	} else {
+		updateActivities(fmt.Sprintf("* Player 'X' can't select cell [%d , %d], the cell is already selected", rowIndex, colIndex))
+	}
 
 	http.Redirect(responseWriter, req, REDIRECT_URL, http.StatusMovedPermanently)
 }
@@ -266,7 +177,7 @@ func onRenderPlayControl(responseWriter http.ResponseWriter, req *http.Request) 
 	log.Println("onRenderPlayControl function called")
 	writeCommonHeaders(responseWriter)
 
-	if isGameStarted(board) {
+	if boardManager.IsGameStarted() {
 		responseWriter.Write(assetManager.GetSvg(assets.RestartButton))
 	} else {
 		responseWriter.Write(assetManager.GetSvg(assets.ComputerStartButton))
@@ -277,14 +188,13 @@ func onPlayControlClick(responseWriter http.ResponseWriter, req *http.Request) {
 	log.Println("onPlayControlClick function called")
 	writeCommonHeaders(responseWriter)
 
-	if isGameStarted(board) {
-		// Restart button clicked
+	if boardManager.IsGameStarted() {
 		log.Println("Reset the board")
-		resetBoard()
+		boardManager.ResetBoard()
 	} else {
-		// 'Let computer play first button' clicked
 		log.Println("Computer making a move")
-		board[1][1] = NOUGHT_CELL // Todo make a random selection
+		computerMoveRowIndex, computerMoveColIndex := boardManager.PlayComputerMove(board.NOUGHT_CELL)
+		updateActivities(fmt.Sprintf("* Player 'O' clicked cell [%d , %d]", computerMoveRowIndex, computerMoveColIndex))
 	}
 
 	http.Redirect(responseWriter, req, REDIRECT_URL, http.StatusMovedPermanently)
@@ -294,12 +204,16 @@ func renderActivities(responseWriter http.ResponseWriter, req *http.Request) {
 	log.Println("renderActivities function called")
 	writeCommonHeaders(responseWriter)
 
-	writeTextResponseAsImage(activities, responseWriter)
+	writeTextResponseAsImage(getActivities(), responseWriter)
 }
 
 // Router methods ends here
 
+const ROW_SIZE = 3
+const COLUMN_SIZE = 3
+
 var assetManager = assets.New()
+var boardManager = board.New(ROW_SIZE, COLUMN_SIZE)
 
 func main() {
 	port := getPort()
